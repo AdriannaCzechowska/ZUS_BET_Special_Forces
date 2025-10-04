1f151115ece9692b6963a353db8a6f47d8656cb8
Dark Knight <smalladam08@gmail.com>
2025-10-04 21:23:31 +0000

    gdzie moge zmienic aby moc rozciagnac te dwie kolumny na pierwszym glown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
-      <main className="flex-grow w-full max-w-4xl mx-auto p-4 sm:p-6 lg:p-8">
+      <main className="flex-grow w-full mx-auto p-4 sm:p-6 lg:p-8">
         <NewPensionSimulator />
       </main>
       <Footer />
