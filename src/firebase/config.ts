abd582836388293f170a48e007648d6e9fa78e8f
Mateusz Czerwiński <matekczer@gmail.com>
2025-10-04 11:30:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-779862535-5072c",
+  "appId": "1:554856219451:web:b1db669d5b474fa7292d1e",
+  "apiKey": "AIzaSyBgJggsm2HkMxcrr2vWC_rfug30Mg7QDGg",
+  "authDomain": "studio-779862535-5072c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "554856219451"
+};
